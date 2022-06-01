517b4e381c21c0810fd43799b0171e0f71cdb8ed
Sangram <sangram15.behera@gmail.com>
2022-06-02 03:54:03 +0530

    Sort Name Alphabetically

---
+++ AddressBookSys/AddressBookSys/AddressBookMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        List<Contact> addressBook = new List<Contact>();
+        public Dictionary<string, List<Contact>> dict = new Dictionary<string, List<Contact>>();
+        public static Dictionary<string, List<Contact>> dictcity = new Dictionary<string, List<Contact>>();
+        public static Dictionary<string, List<Contact>> dictstate = new Dictionary<string, List<Contact>>();
+        Contact contact = new Contact();
+        public void SortingContactsByName()
+        {
+            foreach (var data in addressBook.OrderBy(s => s.FirstName).ToList())
+            {
+                if (addressBook.Contains(data))
+                {
+                    Console.WriteLine("Name of the Person : " + data.FirstName + " " + data.LastName);
+                    Console.WriteLine("Email ID : " + data.Email);
+                    Console.WriteLine("Mobile Number : " + data.PhoneNumber);
+                    Console.WriteLine("Address : " + data.Address);
+                    Console.WriteLine("City : " + data.City);
+                    Console.WriteLine("State : " + data.State);
+                    Console.WriteLine("ZipCode : " + data.ZipCode);
+                    Console.WriteLine("\n");
+                }
+
+            }
+        }
+    }
+}
---
+++ AddressBookSys/AddressBookSys/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AddressBook;
+
+namespace AddressBook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To Address Book Program");
+            bool end = true;
+            Console.WriteLine("SelectNumber\n1)Sorting alphabetically by Person’s name\n2)End Of Program");
+            Contact contact = new Contact();
+            AddressBookMain addContact = new AddressBookMain();
+            while (end)
+            {
+                Console.WriteLine("choose program to exucute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addContact.SortingContactsByName();
+                        break;
+                    case 2:
+                        end = false;
+                        Console.WriteLine("Program Is Ended");
+                        break;
+
+                }
+            }
+        }
+    }
+}
